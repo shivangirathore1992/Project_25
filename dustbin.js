888b9ff2bf952b3a3e479edadcdfd18063290124
shivangirathore1992 <76670230+shivangirathore1992@users.noreply.github.com>
2021-06-25 11:19:38 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -3,8 +3,8 @@ class Dustbin extends BaseClass {
 
         super(x,y,width,height);
         this.image = loadImage("dustbin.png");
-        sprite.scale = 3;
-
+       
+       
     } display() {
          scale(0.5,0.5)
          image(this.image, pos.x, pos.y, this.width, this.height);
